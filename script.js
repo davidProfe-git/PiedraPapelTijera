c626ab18fa746e447d1432e9b545fd962f3e280b
David Rugeles <david.rugeles@pi.edu.co>
2025-10-21 13:01:08 -0500

    cree el scritp pi-pa-ti

---
+++ script.js
@@ -0,0 +1,15 @@
+//variables
+//no pueden empezar con numero ni caracteres especiales $ # -
+//notacion es el tipo de forma en la que escribo variables funciones etc
+//camelCase, snake_case, PascalCase
+//alert("vamos a jugar a piedra papel o tijera")
+let jugador = prompt("¿Cuál escojes? 🪨📄✂️")
+
+//condiciones
+if(jugador === "🪨" || jugador === "📄" || jugador === "✂️"){
+    alert("A jugar!!")
+
+}else{     
+    alert("escogiste una opcion invalida 👨‍🦽")   
+}
+
